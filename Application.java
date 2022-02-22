3e794184ccc38e9d7861a5f8ac530a98c26e6e36
João Jatobá <brandaojatoba@gmail.com>
2022-02-22 00:35:00 -0300

    updated to class

--- Application.java
+++ Application.java
@@ -1,31 +1,21 @@
 import java.util.Locale;
 import java.util.Scanner;
-
 public class Application {
-        
-        public static final double PI = 3.14159;
+              
 
         public static void main(String[] args) {
              Locale.setDefault(Locale.US);   
              Scanner sc = new Scanner(System.in);
+             Calculator calc = new Calculator();
              System.out.print("Entre valor do raio: ");
              double radius = sc.nextDouble();
-             double c = circumference(radius);
-             double v = volume(radius);
+             double c = calc.circumference(radius);
+             double v = calc.volume(radius);
              System.out.printf("Circunference: %.2f%n", c);
              System.out.printf("Volume: %.2f%n", v);
-             System.out.printf("PI value: %.5f%n", PI);
+             System.out.printf("PI value: %.5f%n", calc.PI);
 
              sc.close();
         }
 
-        public static double circumference(double radius){
-                
-                return 2.0 * PI * radius;
-
-        }
-
-        public static double volume(double radius){
-                return 4.0 * PI * radius * radius * radius / 3;
-        }
 }
